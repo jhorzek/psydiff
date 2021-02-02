38c3abf2572e83af18118bd7bfa48f8cfa71347b
Jannik <orzek@mpib-berlin.mpg.de>
2021-02-02 19:00:57 +0100

    working for linear model

---
+++ R/modelTemplate.R
@@ -0,0 +1,390 @@
+modelTemplate <- function(){
+  return('
+  // [[Rcpp::depends(RcppArmadillo, psydiff)]]
+
+#include <psydiff.h>
+#include <RcppArmadillo.h>
+
+/* The type of container used to hold the state vector */
+typedef arma::mat state_type;
+
+// By default, odeint will not work with armadillo
+// The following code is copied from headmyshouder
+// and allows the type of the states to be arma::mat
+// https://stackoverflow.com/questions/41753823/armadillo-conflicts-with-boost-odeint-odeint-resizes-the-state-vector-to-zero-d
+namespace boost { namespace numeric { namespace odeint {
+// template for arma::mat - define as resizable
+template <>
+struct is_resizeable<arma::mat>
+{
+  typedef boost::true_type type;
+  const static bool value = type::value;
+};
+
+// define size comparisons for two arma::mat
+template <>
+struct same_size_impl<arma::mat, arma::mat>
+{
+  static bool same_size(const arma::mat& X, const arma::mat& Y)
+  {
+    return (X.n_cols == Y.n_cols && X.n_rows == Y.n_rows);
+  }
+};
+
+// define resize command for arma::mat
+template<>
+struct resize_impl<arma::mat, arma::mat>
+{
+  static void resize(arma::mat& X, const arma::mat& Y)
+  {
+    X.reshape(Y.n_rows, Y.n_cols);
+  }
+};
+
+} } } // namespace boost::numeric::odeint
+
+// Next, we set up the structs in which the parameters
+// and internal information for odeint are saved
+
+struct odeintpars{
+  Rcpp::List parameterList, additional;
+  arma::mat meanWeights, covWeights, W;
+  int nlatent, nmanifest, person;
+  double c, alpha, beta, kappa;
+};
+
+MEASUREMENTEQUATIONPLACEHOLDER
+
+arma::mat getY_(const arma::mat sigmaPoints, const odeintpars &pars,
+                const int &person,
+                const double &t){
+  arma::mat Y_(pars.nlatent, sigmaPoints.n_cols, arma::fill::zeros);
+  arma::colvec currentSigma;
+  for(int co = 0; co < sigmaPoints.n_cols; co++){
+    currentSigma = sigmaPoints.col(co);
+    Y_.col(co) = measurementEquation(currentSigma, pars,
+           person, t);
+  }
+  return Y_;
+}
+
+LATENTEQUATIONPLACEHOLDER
+
+arma::mat computeL(const odeintpars &pars,
+                   const int &person,
+                   const double &t){
+  Rcpp::List modelPars = pars.parameterList;
+  arma::mat L = modelPars["L"];
+  return(L);
+}
+
+arma::mat getMMatrix(const arma::mat &sigmaPoints, const odeintpars &pars,
+                     const int &person,
+                     const double &t){
+  // Note: This function has to be compiled later on as the users are supposed to
+  // provide their own models. The current implementation is a placeholder.
+  // sigmaPoints are called x in Särkkä (2007)
+
+  arma::mat M, Qc, invCov;
+  arma::mat sigmaPredict(sigmaPoints.n_rows, sigmaPoints.n_cols, arma::fill::zeros);
+  arma::mat A =  getAFromSigmaPoints_C(sigmaPoints,
+                                       pars.meanWeights,
+                                       pars.c);
+  invCov = arma::inv(A);
+
+
+  // Qc
+  // Qc is the diffusion matrix of the Brownian Motion.
+  // In our case this corresponds to the identity matrix (standard Brownian motion).
+  // Modulation of the Brownian Motion is "outsourced" to Matrix L
+  Qc = arma::eye(pars.nlatent, pars.nlatent);
+
+  for(int co = 0; co < sigmaPoints.n_cols; co++){
+    sigmaPredict.col(co) = latentEquation(sigmaPoints.col(co), pars,
+                     person, t);
+  }
+
+  arma::colvec m_x(pars.nlatent, 1, arma::fill::zeros);
+  arma::colvec m_f(pars.nlatent, 1, arma::fill::zeros);
+  for(int co = 0; co < sigmaPoints.n_cols; co++){
+    m_x += pars.meanWeights(co) * sigmaPoints.col(co);
+    m_f += pars.meanWeights(co) * sigmaPredict.col(co);
+  }
+
+  // evaluate summations
+  arma::mat sum1(pars.nlatent, pars.nlatent, arma::fill::zeros);
+  arma::mat sum2(pars.nlatent, pars.nlatent, arma::fill::zeros);
+  for(int co = 0; co < sigmaPoints.n_cols; co++){
+    sum1 += pars.covWeights(co) * (sigmaPoints.col(co) - m_x) * arma::trans(sigmaPredict.col(co) - m_f);
+    sum2 += pars.covWeights(co) * (sigmaPredict.col(co) - m_f)*arma::trans(sigmaPoints.col(co) - m_x);
+  }
+
+  arma::mat L = computeL(pars, person, t);
+
+  // compute M
+  M = invCov*(
+    sum1+
+    sum2+
+    L*Qc*arma::trans(L)
+  )*arma::trans(invCov);
+  return(M);
+}
+
+// Model for integration
+class odeintModel{
+  struct odeintpars modelParameters;
+public:
+  odeintModel(struct odeintpars newParameters) : modelParameters(newParameters){}
+  void operator()(const state_type &x , state_type &dxdt , const double  t ){
+    arma::mat A = getAFromSigmaPoints_C(x, modelParameters.meanWeights, modelParameters.c);
+    // compute M matrix
+    arma::mat M = getMMatrix(x, modelParameters, modelParameters.person, t);
+
+    arma::mat phi_M = getPhi_C(M);
+
+    arma::mat augmentedMatrix(x.n_rows, x.n_cols, arma::fill::zeros);
+    augmentedMatrix.submat(0,1,modelParameters.nlatent-1,modelParameters.nlatent) = A*phi_M;
+    augmentedMatrix.submat(0,modelParameters.nlatent+1,modelParameters.nlatent-1,2*modelParameters.nlatent) = -1*A*phi_M;
+
+    arma::mat sigmaPredict(x.n_rows, x.n_cols, arma::fill::zeros);
+
+    for(int co = 0; co < x.n_cols; co++){
+      sigmaPredict.col(co) = latentEquation(x.col(co), modelParameters, modelParameters.person, t);
+    }
+
+    for(int co = 0; co < dxdt.n_cols; co++){
+      dxdt.col(co) = sigmaPredict * modelParameters.meanWeights + sqrt(modelParameters.c)*augmentedMatrix.col(co);
+    }
+  }
+};
+
+// [[Rcpp::export]]
+Rcpp::List fitModel(Rcpp::List panelSDEModel, double alpha = 0.6, double beta = 2.0, double kappa = -1.0,
+                    double timeStep = 0.01, std::string integrateFunction = "default", int verbose = 0){
+
+  // extract parameters from model
+  Rcpp::List pars = panelSDEModel["pars"];
+  Rcpp::List parameterList = pars["parameterList"]; // has all starting values
+  Rcpp::DataFrame parameterTable = Rcpp::as<Rcpp::DataFrame>(pars["parameterTable"]);
+  Rcpp::NumericVector personInParameterTable = parameterTable["person"]; // persons
+  Rcpp::List additional = panelSDEModel["additional"];
+
+  odeintpars odeintparam;
+  odeintparam.parameterList = parameterList;
+  odeintparam.nlatent = panelSDEModel["nlatent"];
+  odeintparam.nmanifest = panelSDEModel["nmanifest"];
+  odeintparam.alpha = alpha;
+  odeintparam.beta = beta;
+  odeintparam.kappa = kappa;
+  if(odeintparam.nlatent + odeintparam.kappa == 0){
+    Rcpp::warning("Setting of kappa results in division by 0. Kappa was changed to kappa - 1");
+    kappa -= 1;
+    odeintparam.kappa = kappa;
+  }
+  double c = pow(odeintparam.alpha,2)*(odeintparam.nlatent + odeintparam.kappa);
+  odeintparam.c = c;
+
+  // data
+  Rcpp::List data = panelSDEModel["data"];
+  Rcpp::NumericVector personsInData = data["person"]; // persons
+  Rcpp::NumericVector uniquePersons = unique(personsInData);
+  int sampleSize = uniquePersons.length();
+  arma::mat observations = data["observations"]; // observations
+  arma::colvec dt = data["dt"]; // dt
+
+  // changed
+  Rcpp::LogicalVector changed = parameterTable["changed"];
+
+  // initialize Unscented matrices
+  int numsteps, nObservedVariables, timePoints;
+  arma::mat individualObservations, // data for one individual
+  Y_(odeintparam.nmanifest, 2*odeintparam.nlatent+1), // predicted observations from sigma points
+  P(odeintparam.nlatent, odeintparam.nlatent), // updated latent covariance
+  P_(odeintparam.nlatent, odeintparam.nlatent), // latent covariance
+  A(odeintparam.nlatent, odeintparam.nlatent), // root of (updated) latent covariance
+  S(odeintparam.nmanifest,odeintparam.nmanifest), // predicted observed covariance
+  C(odeintparam.nlatent, odeintparam.nmanifest), // predicted cross-covariance
+  K(odeintparam.nlatent, odeintparam.nmanifest), // Kalman Gain
+  latentScores(observations.n_rows, odeintparam.nlatent, arma::fill::zeros), // collects predicted latent scores
+  predictedManifest(observations.n_rows, odeintparam.nmanifest, arma::fill::zeros); // collects predicted observations
+  //individualLatentScores,
+  //individualPredictedManifest;
+  arma::colvec individualDts, // person specific dt
+  m_(odeintparam.nlatent), // predicted latent means
+  m(odeintparam.nlatent), // updated latent means
+  mu(odeintparam.nmanifest), // predicted manifest means
+  individualXTimeObservations, // for the observed data of a person at a specific time point
+  observedNotNA, // will be used to store observations without missings
+  residual, // difference between observed and predicted
+  m2LL(sampleSize, arma::fill::zeros); // individual likelihoods
+  arma::uvec nonmissing, // vector for indices of nonmissing data
+  missing; // vector for indices of missing data
+
+  // define type of state
+  state_type x;
+
+  // initialize start and end time of integration
+  double startTime, endTime;
+
+  // iterate over all persons
+  for(int person = 0; person < sampleSize; person++){
+    int selectedPerson = uniquePersons(person);
+    odeintparam.person = selectedPerson;
+    // check for this person if any parameters changed
+    Rcpp::LogicalVector parschanged = changed[personInParameterTable == selectedPerson];
+
+    if(is_false(any(parschanged))){
+      // if nothing changed, we can skip this person
+      if(verbose == 1){Rcpp::Rcout << "Skipping person " << selectedPerson << std::endl;}
+      continue;
+    }
+    // if something changed, set parameters
+    // Step 1: Set parameters
+    // (will pass by reference and change the parameters directly in the parameterList)
+    setParameterTable_C(parameterTable, parameterList, selectedPerson);
+
+    // extract initial parameters
+    m = Rcpp::as<arma::colvec>(parameterList["m0"]);
+    P = Rcpp::as<arma::mat>(parameterList["A0"])*arma::trans(Rcpp::as<arma::mat>(parameterList["A0"]));
+    arma::mat Rchol = odeintparam.parameterList["Rchol"];
+    arma::mat R = Rchol*arma::trans(Rchol); // manifest covariance
+
+    // extract individual observations
+    arma::uvec rowInd = arma::find(Rcpp::as<arma::rowvec>(personsInData) == selectedPerson);
+    individualObservations = observations.rows(rowInd);
+    individualDts = dt.rows(rowInd);
+    //individualPredictedManifest = predictedManifest.rows(rowInd);
+    //individualLatentScores = latentScores.rows(rowInd);
+
+    // iterate over all time points
+    double timeSum = 0.0;
+    timePoints = individualDts.n_elem;
+    for(int timePoint = 0; timePoint < timePoints; timePoint++){
+      if(verbose == 1){Rcpp::Rcout << "Fitting Person" <<  selectedPerson << " time " << timePoint << std::endl;}
+      timeSum += individualDts(timePoint);
+
+      // extract individual- and time-specific data as colvec
+      individualXTimeObservations = arma::trans(individualObservations.row(timePoint));
+
+      // find non-missing
+      nonmissing = arma::find_finite(individualXTimeObservations);
+      missing = arma::find_nonfinite(individualXTimeObservations);
+      nObservedVariables = nonmissing.size();
+
+      observedNotNA = individualXTimeObservations(nonmissing);
+
+      // set latent mean and covariance as well as root of covariance
+      m_ = m;
+      P_ = P;
+      A = chol(P_, "lower");
+
+      // PREDICTION
+
+      // Compute sigma-points
+      x = getSigmaPoints_C(m_, A, true, odeintparam.c);
+
+      // Compute mean weights, cov weights and weight matrix M
+      arma::colvec meanWeights = getMeanWeights_C(odeintparam.nlatent, odeintparam.alpha, odeintparam.beta, odeintparam.kappa);
+      odeintparam.meanWeights = meanWeights;
+      arma::colvec covWeights = getCovWeights_C(odeintparam.nlatent, odeintparam.alpha, odeintparam.beta, odeintparam.kappa);
+      odeintparam.covWeights = covWeights;
+      arma::mat W = getWMatrix_C(meanWeights, covWeights);
+      odeintparam.W = W;
+
+      // Set up model
+      odeintModel individualOdeintModel(odeintparam);
+
+      // Integrate
+      startTime = 0.0;
+      endTime = individualDts(timePoint);
+
+      if(abs(startTime - endTime) > 0){
+
+        if(integrateFunction == "rk4"){
+          boost::numeric::odeint::runge_kutta4< state_type > stepper;
+          numsteps = boost::numeric::odeint::integrate_const(stepper, individualOdeintModel, x,
+                                                             startTime, endTime,
+                                                             timeStep);//, push_back_state_and_time( x_vec , times ));
+        }else{
+          numsteps = boost::numeric::odeint::integrate(individualOdeintModel, x,
+                                                       startTime, endTime,
+                                                       timeStep);
+        }
+        m_ = x.col(0);
+        P_ = computePFromSigmaPoints_C(x, meanWeights, c);
+      }
+
+      // MANIFEST PREDICTION
+      Y_ = getY_(x, odeintparam,
+                 selectedPerson,
+                 timeSum);
+
+      // expected manifest mean
+      mu = predictMu_C(Y_, meanWeights);
+
+      // save prediction
+      predictedManifest.row(min(rowInd) + timePoint) = arma::trans(mu);
+
+      if(nObservedVariables > 0){
+        // expected manifest covariance
+        S = predictS_C(Y_.rows(nonmissing), odeintparam.W, R.submat(nonmissing, nonmissing));
+
+        // make symmetric if rounding errors lead to non-symmetry
+        S = (S + arma::trans(S))/2;
+
+        // expected manifest cross-covariance
+        C = predictC_C(x, Y_.rows(nonmissing), odeintparam.W);
+
+        // Kalman Gain
+        K = computeK_C(C, S);
+
+        // compute residuals
+        residual = individualXTimeObservations - mu;
+
+        // UPDATE
+        // Update latent mean
+        m = updateM_C(m_, K, residual(nonmissing));
+        latentScores.row(min(rowInd) + timePoint) = arma::trans(m);
+
+        // Update latent covariance
+        P = updateP_C(P_, K, S);
+        // make symmetric if rounding errors lead to non-symmetry
+        P = (P + arma::trans(P))/2;
+
+        // compute Likelihood --nonmissing
+
+        arma::uvec m2LLInd = arma::find(Rcpp::as<arma::rowvec>(uniquePersons) == selectedPerson);
+        m2LL.elem(m2LLInd) += computeIndividualM2LL_C(nObservedVariables,
+                  individualXTimeObservations(nonmissing),
+                  mu(nonmissing), S);
+
+      }else{
+        // if all data is missing / no update is requested
+        m = m_;
+        latentScores.row(min(rowInd) + timePoint) = arma::trans(m);
+
+        P = P_;
+        // make symmetric if rounding errors lead to non-symmetry
+        P = (P + arma::trans(P))/2;
+      }
+    }
+  }
+
+  Rcpp::List ret  = Rcpp::List::create(Rcpp::Named("latentScores") = latentScores,
+                                       Rcpp::Named("predictedManifest") = predictedManifest,
+                                       Rcpp::Named("m2LL") = m2LL,
+                                       Rcpp::Named("sigmaPoints") = x,
+                                       Rcpp::Named("Y_") = Y_,
+                                       Rcpp::Named("mu") = mu,
+                                       Rcpp::Named("S") = S,
+                                       Rcpp::Named("C") = C,
+                                       Rcpp::Named("K") = K,
+                                       Rcpp::Named("m_") = m_,
+                                       Rcpp::Named("P_") = P_,
+                                       Rcpp::Named("m") = m,
+                                       Rcpp::Named("P") = P);
+  return(ret);
+}
+
+  ')
+}
---
+++ R/prepareModel.R
@@ -0,0 +1,296 @@
+panelsde <- function(dataset, latentEquations, manifestEquations, L, Rchol, A0, m0,
+                     grouping = NULL, parameters, groupingvariables = NULL,
+                     additional = NULL, compile = TRUE){
+  nlatent <- ncol(L)
+  nmanifest <- ncol(Rchol)
+
+  # checks
+  checkEquation(latentEquations)
+  checkEquation(manifestEquations)
+  if(!is.list(dataset)){
+    stop("dataset must be of class list")
+  }
+  datasetNames <- names(dataset)
+  for(datasetName in datasetNames){
+    if(!(datasetName %in% c("person", "observations", "dt"))){
+      stop("Unknown field in dataset. Dataset must have the fields 'person',
+           'observations', 'dt'")
+    }
+  }
+  if(!ncol(dataset[["observations"]]) == nmanifest){
+    stop("Dataset and R differ in their number of manifest variables.")
+  }
+  if(!is.list(parameters)){
+    stop("parameters must be of class data.frame")
+  }
+  if(!(is.null(additional) || is.list(additional))){
+    stop("additional must be of class data.frame or NULL")
+  }
+
+  ## Extract persons
+  persons <- dataset$person
+
+  ## Define matrices, vectors, etc.
+  ## Element refers to containers in which the parameters are stored
+  parameters$L <- L
+  parameters$A0 <- A0
+  parameters$m0 <- m0
+  parameters$Rchol <- Rchol
+
+  elementNames <- names(parameters)
+
+  ## initialize parameterTable and starting values. parameterTable will hold the
+  # parameter values for each individual and tell other functions,
+  # where those parameters can be found (e.g., a4 is in matrix A in row
+  # 1, col 1)
+  parTabAndStart <- extractParameters(elementNames = elementNames, parameters = parameters)
+  parameterTableInit <- parTabAndStart$parameterTableInit
+  parameterList <- parTabAndStart$startValues
+
+  parameterTable <- do.call("rbind", replicate(length(unique(persons)), parameterTableInit, simplify = FALSE))
+  parameterTable$person <- rep(unique(persons), each = nrow(parameterTableInit))
+
+  ## check groupings
+  parameterTable <- makeGroups(grouping = grouping, parameterTable = parameterTable)
+
+  ## prepare equations
+  manifestEquationsStart <- "
+  arma::colvec measurementEquation(const arma::colvec &x, const odeintpars &pars,
+                                                             const int &person,
+                                                             const double &t){
+    arma::colvec y(pars.nmanifest, arma::fill::zeros);
+  "
+  manifestEquationsMiddle <- prepareEquations(equations = manifestEquations,
+                                              parameters = parameters)
+  manifestEquationsEnd <- "
+    return(y);
+  }
+  "
+  manifestEquationsCpp <- paste0(manifestEquationsStart, manifestEquationsMiddle, manifestEquationsEnd)
+
+  latentEquationsStart <- "
+  arma::colvec latentEquation(const arma::colvec &x, const odeintpars &pars,
+                            const int &person,
+                            const double &t){
+                            arma::colvec dx(pars.nlatent, arma::fill::zeros);
+  "
+  latentEquationsMiddle <- prepareEquations(equations = latentEquations,
+                                            parameters = parameters)
+  latentEquationsEnd <- "
+    return(dx);
+  }
+  "
+  latentEquationsCpp <- paste0(latentEquationsStart, latentEquationsMiddle, latentEquationsEnd)
+
+  ## set up model
+  modelCpp <- modelTemplate()
+  modelCpp <- stringr::str_replace_all(modelCpp, "MEASUREMENTEQUATIONPLACEHOLDER", manifestEquationsCpp)
+  modelCpp <- stringr::str_replace_all(modelCpp, "LATENTEQUATIONPLACEHOLDER", latentEquationsCpp)
+
+  ## compile
+  if(compile){
+    filename <- tempfile(pattern = "panelsde_", tmpdir = tempdir(), fileext = ".cpp")
+    fileConn<-file(filename)
+    writeLines(modelCpp, fileConn)
+    close(fileConn)
+    cat("Compiling model...")
+    Rcpp::sourceCpp(file = filename)
+    cat("Done.\n The model can be fitted with the fitModel()-function and the returned object.")
+  }
+
+  ## return model
+  pars <- list("parameterList" = parameterList,
+               "parameterTable" = parameterTable
+  )
+  panelSDEModel <- list("pars" = pars,
+                        "nlatent" = nlatent,
+                        "nmanifest" = nmanifest,
+                        "data" = dataset,
+                        "additional"= additional
+  )
+
+  if(compile){
+    return(panelSDEModel)
+  }
+
+  return(list("panelSDEModel" = panelSDEModel, "cppCode" = modelCpp))
+
+}
+
+checkEquation <- function(equation){
+  if(!stringr::str_detect(equation, ";", negate = FALSE)){
+    stop(paste0("Expected each command in ...\n", equation, "\n to end with ; "))
+  }
+}
+
+extractParameters <- function(elementNames, parameters){
+  parameterLabels <- c()
+  parameterValues <- c()
+  parameterTargets <- c()
+  parameterTargetClass <- c()
+  parameterRow <- c()
+  parameterCol <- c()
+  parameterChanged <- c()
+
+  startValues <- parameters
+  for(elementName in elementNames){
+    currentElement <- parameters[[elementName]]
+    if(is.matrix(currentElement)){
+      elementValues <- matrix(NA, nrow(currentElement), ncol(currentElement))
+      for(ro in 1:nrow(currentElement)){
+        for(co in 1:ncol(currentElement)){
+          matrixelement <- currentElement[ro,co]
+          if(!stringr::str_detect(matrixelement, "=", negate = FALSE)){
+            elementValues[ro,co] <- as.numeric(matrixelement)
+            next
+          }
+          matrixelementSplit <- stringr::str_split(matrixelement, "=")[[1]]
+          matrixelementSplit <- stringr::str_remove_all(matrixelementSplit, " ")
+          elementValues[ro,co] <- as.numeric(matrixelementSplit[2])
+          parameterLabels <- c(parameterLabels, matrixelementSplit[1])
+          parameterValues <- c(parameterValues, as.numeric(matrixelementSplit[2]))
+          parameterTargets <- c(parameterTargets, elementName)
+          parameterTargetClass <- c(parameterTargetClass, "matrix")
+          parameterRow <- c(parameterRow, ro-1)
+          parameterCol <- c(parameterCol, co-1)
+          parameterChanged <- c(parameterChanged, TRUE)
+        }
+      }
+    }else if(is.numeric(currentElement)){
+      elementValues <- currentElement
+      parameterLabels <- c(parameterLabels, elementName)
+      parameterValues <- c(parameterValues, currentElement)
+      parameterTargets <- c(parameterTargets, elementName)
+      parameterTargetClass <- c(parameterTargetClass, "double")
+      parameterRow <- c(parameterRow, 0)
+      parameterCol <- c(parameterCol, 0)
+      parameterChanged <- c(parameterChanged, TRUE)
+    }else if(is.vector(currentElement)){
+      warning(paste0(elementName, " is of type vector. It will be interpreted as column vector.
+                     Please make sure that this is considered in your model definition."))
+      elementValues <- rep(NA, length(currentElement))
+      for(ro in 1:length(currentElement)){
+        matrixelement <- currentElement[ro]
+        if(!stringr::str_detect(matrixelement, "=", negate = FALSE)){
+          elementValues[ro] <- as.numeric(matrixelement)
+          next
+        }
+        matrixelementSplit <- stringr::str_split(matrixelement, "=")[[1]]
+        matrixelementSplit <- stringr::str_remove_all(matrixelementSplit, " ")
+        elementValues[ro] <- as.numeric(matrixelementSplit[2])
+        parameterLabels <- c(parameterLabels, matrixelementSplit[1])
+        parameterValues <- c(parameterValues, as.numeric(matrixelementSplit[2]))
+        parameterTargets <- c(parameterTargets, elementName)
+        parameterTargetClass <- c(parameterTargetClass, "colvec")
+        parameterRow <- c(parameterRow, ro-1)
+        parameterCol <- c(parameterCol, 0)
+        parameterChanged <- c(parameterChanged, TRUE)
+      }
+    }else{
+      stop(paste0("Unknown data type for parameter ", currentElement, ". Allowed are numeric, matrix, and vector."))
+    }
+    startValues[[elementName]] <- elementValues
+  }
+
+  parameterTableInit <- data.frame("label" = parameterLabels,
+                                   "value" = parameterValues,
+                                   "target" = parameterTargets,
+                                   "targetClass" = parameterTargetClass,
+                                   "row" = parameterRow,
+                                   "col" = parameterCol,
+                                   "changed" = parameterChanged)
+  return(list("parameterTableInit" = parameterTableInit,
+              "startValues" = startValues))
+
+}
+
+makeGroups <- function(grouping, parameterTable){
+  if(!is.null(grouping)){
+    checkEquation(grouping)
+    ## split groupings
+    grouping <- stringr::str_remove_all(grouping, "\n")
+    grouping <- stringr::str_remove_all(grouping, " ")
+    splitgrouping <- stringr::str_split(grouping, ";")[[1]]
+    splitgrouping <- splitgrouping[!splitgrouping == ""]
+
+    for(grp in splitgrouping){
+      grpsplit <- stringr::str_split(grp, "\\|")[[1]]
+      parname <- grpsplit[1]
+      if(grpsplit[2] %in% c("person", "persons", "individual", "individuals")){
+        for(p in unique(persons)){
+          parameterTable[parameterTable$label == parname & parameterTable$person == p,"label"] <- paste0(parname, "_p", p)
+        }
+      }else{
+        ## user-specified splitting
+        groupingvariableNames <- names(groupingvariables)
+        if(!grpsplit[2] %in% groupingvariableNames){
+          stop(paste0("Grouping variable ", grpsplit[2], " not found in groupingvariables"))}
+        groupingIndicator <- groupingvariables[[grpsplit[2]]]
+        for(p in unique(persons)){
+          parameterTable[parameterTable$label == parname & parameterTable$person == p,"label"] <- paste0(parname, "_p", groupingIndicator[p])
+        }
+      }
+    }
+  }
+  return(parameterTable)
+}
+
+
+prepareEquations <- function(equations, parameters){
+  ## remove any special symbols (+, +, ...)
+  elementsInEquations <- stringr::str_split(equations, '[^[:alnum:]]')[[1]]
+  elementsInEquations <- elementsInEquations[!elementsInEquations== ""]
+  equationsCombined <- "Rcpp::List modelPars = pars.parameterList;
+  "
+
+  targets <- names(parameters)
+
+  for(elementInEquation in elementsInEquations){
+    if(elementInEquation %in% targets){
+      targetClass <- parameters[[elementInEquation]]
+      if(is.matrix(targetClass)){
+        equationsCombined <- paste0(equationsCombined, '
+               arma::mat ', elementInEquation, ' = modelPars["',elementInEquation,'"]; \n')
+      }else if(is.vec(targetClass)){
+        equationsCombined <- paste0(equationsCombined, '
+               arma::colvec ', elementInEquation, ' = modelPars["',elementInEquation,'"]; \n')
+      }else if(is.numeric(targetClass)){
+        equationsCombined <- paste0(equationsCombined, '
+               double ', elementInEquation, ' = modelPars["',elementInEquation,'"]; \n')
+      }else{
+        stop("Error in prepareEquations: targetClass unknown.")
+      }
+    }
+  }
+  equationsCombined <- paste0(equationsCombined, "
+                              ", equations)
+  return(equationsCombined)
+}
+
+fromMxMatrix <- function(mxMatrixObject){
+  values <- as.character(mxMatrixObject$values)
+  labels <- as.character(mxMatrixObject$labels)
+  values[!is.na(labels)] <- paste0(labels[!is.na(labels)], " = ", values[!is.na(labels)])
+  values <- matrix(values,
+                   nrow = nrow(mxMatrixObject$values),
+                   ncol = ncol(mxMatrixObject$values))
+  return(values)
+
+}
+
+sdeModelMatrix <- function(values, labels){
+  if(!(nrow(values) == nrow(labels) && ncol(values) == ncol(labels))){
+    stop("values and labels have different dimensions")
+  }
+  ro <- nrow(values)
+  co <- ncol(values)
+  values <- as.character(values)
+  labels <- as.character(labels)
+  naOrEmpty <- is.na(labels) | labels == ""
+  values[!naOrEmpty] <- paste0(labels[!naOrEmpty], " = ", values[!naOrEmpty])
+  values <- matrix(values,
+                   nrow = ro,
+                   ncol = co)
+  return(values)
+
+}
